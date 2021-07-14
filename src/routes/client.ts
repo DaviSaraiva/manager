8330deec79e4ee7e3a3342043c233699e6d6c533
DaviSaraiva <davi123467890@gmail.com>
2021-07-14 16:37:57 -0300

    finalizcao do delete

--- src/routes/client.ts
+++ src/routes/client.ts
@@ -8,7 +8,7 @@ clientRoutes.get('/',clientController.index);
 clientRoutes.get('/paginated',clientController.paginated);
 clientRoutes.get('/searchname',clientController.search);
 clientRoutes.post('/',clientController.create);
-clientRoutes.put('/:id',clientController.update); 
+clientRoutes.put('/update/:id',clientController.update); 
 clientRoutes.delete('/delete/:id',clientController.destroy); 
 
 
