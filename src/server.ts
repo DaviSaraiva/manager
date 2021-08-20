1149b98d1cfc513d34740e4582e657a875a5ba3d
DaviSaraiva <davi123467890@gmail.com>
2021-08-20 14:45:59 -0300

    add o cors

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,10 @@ import express, { Request, Response, NextFunction } from 'express';
 import './database';
 import routes from './routes';
 import AppError from './errors/AppError';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use((err:Error, request: Request, response: Response, _:NextFunction )=>{
